ba7f719275d4f5a9a035ca509bf12ba2cf8b046a
John Jackson <jmcjack@gmail.com>
2014-03-27 00:03:23 -0500

    Removed 2012 Directory

--- 2012/FlightCode/fc_compile2013.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "C:\Documents and Settings\Minnesat\Desktop\HAXDT\Dropbox\HASP\2013\Flight Code"
-make all 2>"C:\Documents and Settings\Minnesat\Desktop\HAXDT\Dropbox\HASP\2013\Flight Code\error.txt"
