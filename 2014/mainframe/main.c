5f416d1e33ab872abdb26108e9507aad9d1be147
John Jackson <jmcjack@gmail.com>
2014-06-03 15:23:19 -0500

    Test commit!

---
+++ 2014/mainframe/main.c
@@ -0,0 +1,47 @@
+#include <stdio>
+#include <stdlib>
+
+void main() {
+
+	// Initializations //
+	
+	// Set up SPI buses
+		// ADC
+		// IMU
+		// SD Card
+		
+	// Set up serial buses
+		// GPS serial
+		// Telemetry serial
+		
+	// Set up interrupts
+		// 3 GPIOs
+		
+	// Set up other GPIOs
+		// GPS PPS
+		// GPS Lock
+		// Soft reset pins
+		
+	// If no errors, downlink an OKGO
+
+	while(1)
+	{
+		// Check Interrupt Flags
+
+		
+		// Data Acquisition //
+
+
+		// Telemetry //
+
+		
+		// Datalogging //
+
+		
+		// Error Checking
+	
+	} // End main while loop
+	
+	return;
+	
+} // End main function
