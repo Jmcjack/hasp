edcfebc1272115ba57e5ce0733fe5f88c14e4523
John Jackson <jmcjack@gmail.com>
2014-07-22 21:13:00 -0500

    Hashed out the RTC driver.  Needs some fine-tuning and testing.

--- 2014/mainframe/sensors/rtc/hasp_rtc.h
+++ 2014/mainframe/sensors/rtc/hasp_rtc.h
@@ -2,8 +2,7 @@
 #define HASP_RTC_H_
 #define BMA180_I2C_BUFFER 0x80
 
-#define I2C_PORT "/dev/i2c-" // change to actual i2c port
-#define I2C_SLAVE // something, but what?
-#define I2C_Address // something, but what?
+#define I2C_PORT "/dev/i2c-" // change to actual i2c /dev port
+#define I2C_Address  0b1101000 // binary address of the RTC
 
 int getTimeStamp();
