3606ba59d755d6098837fb796ccb40bdfb212eb7
Yunus Agamawi <agama004@umn.edu>
2015-04-08 12:41:25 -0500

    changed gps_novatel.h to be used for testing with gps_novatel_test.c

--- 2015/src/gps/gps_novatel.h
+++ 2015/src/gps/gps_novatel.h
@@ -2,13 +2,15 @@
 #define GPS_NOVATEL_H_
 
 #include <stdint.h>
-#include "globaldefs.h"
+//#include "globaldefs.h"
 
 #define GPS_RST 61
 #define GPS_POS_VAL 44
 
-void init_GPS(struct gps *gpsData_ptr);
-int read_GPS(struct gps *gpsData_ptr);
+//void init_GPS(struct gps *gpsData_ptr);
+//int read_GPS(struct gps *gpsData_ptr);
+int init_GPS();
+int read_GPS();
 void endian_swap(uint8_t *buf, int index, int count);
 unsigned int CRC32Value(int i);
 unsigned int CalculateBlockCRC32(unsigned int ulCount, unsigned char *ucBuffer);
--- 2015/src/gps/gps_novatel_test.c
+++ 2015/src/gps/gps_novatel_test.c
@@ -9,20 +9,41 @@
 #include <string.h>
 #include <math.h>
 
-#include "globaldefs.h"
-#include "simple_gpio.h"
+//#include "globaldefs.h"
+//#include "simple_gpio.h"
 #include "gps_novatel.h"
 
 #define CRC32_POLYNOMIAL   0xEDB88320L
 #define GPS_MAX_MSG_SIZE 300
 #define PV_PIN 44
 #define header_length 28
-#define GPS_PORT "/dev/ttyO5"
+#define GPS_PORT "/dev/usb0"
 
 unsigned char response[512];
 const char asterisk[] = "*";
 
-void init_GPS(struct gps *gpsData_ptr)
+void main() {
+	
+	int fd, res;
+	fd = init_GPS();
+	if (fd < 0)
+	{
+		printf("Failed to initialize GPS.  Returning.\n");
+		return;
+	}
+	printf("Open success\n");
+	while (res != -1)
+	{
+		fprintf(stderr,"Iteration begin");
+		res = read_GPS(fd);
+		printf("Iteration done\n");
+	}
+	printf("Exception - exiting program.\n");
+	
+	return;
+}
+
+int init_GPS()
 {
 
         //uart5 configuration using termios
@@ -31,31 +52,31 @@ void init_GPS(struct gps *gpsData_ptr)
 
          //open uart5 for tx/rx, not controlling device
         if((fd = open(GPS_PORT, O_RDWR | O_NOCTTY)) < 0) {
-                //fprintf(stderr,"Unable to open UART5 access.\n");}
-	}
+                fprintf(stderr,"Unable to open UART5 access.\n");}
+	
         //get attributes of uart5
         if(tcgetattr(fd, &uart5) < 0) {
-                //fprintf(stderr,"Could not get attributes of UART5 at ttyO1\n");}
-	}
+                fprintf(stderr,"Could not get attributes of UART5 at usb0\n");}
+	
         //set Baud rate
         if(cfsetospeed(&uart5, B115200) < 0) {
         
-	}        //fprintf(stderr,"Could not set baud rate\n");}
+	        fprintf(stderr,"Could not set baud rate\n");}
         else {
-              	//fprintf(stderr,"\n\nBaud rate: 115200\n");
-	}
+              	fprintf(stderr,"\n\nBaud rate: 115200\n");}
+	
 
         //set attributes of uart5
         uart5.c_iflag = 0;
         uart5.c_oflag = 0;
         uart5.c_lflag = 0;
         tcsetattr(fd, TCSANOW, &uart5);
-        gpsData_ptr->port = fd;
+        //gpsData_ptr->port = fd;
         //gpsData_ptr->navValid = 0;
-	gpsData_ptr->badDataCounter = 0;
-	gpsData_ptr->posValFlag = 0;
-	gpsData_ptr->lastPosVal = 0;
-	return;
+	//gpsData_ptr->badDataCounter = 0;
+	//gpsData_ptr->posValFlag = 0;
+	//gpsData_ptr->lastPosVal = 0;
+	return fd;
 }
 
 
@@ -106,7 +127,7 @@ unsigned int CalculateBlockCRC32(unsigned int ulCount, unsigned char *ucBuffer)
 }
 
 
-int read_GPS(struct gps *gpsData_ptr)
+int read_GPS(fd)
 {
 
 	unsigned int state = 0;
@@ -115,12 +136,16 @@ int read_GPS(struct gps *gpsData_ptr)
 	int i=0;
 	int pCount=0;
 	int bytesInLocalBuffer;
+	
+	double Xe;
+	double Ye;
+	double Ze;
 
 	unsigned long CRC_computed;
 	unsigned long CRC_read;
 	unsigned int CRC_readstr[4];
 
-	bytesInLocalBuffer = read(gpsData_ptr->port, &response[0],512);
+	bytesInLocalBuffer = read(fd, &response[0],512);
 
 	//for(i=0;i<144;i++) {
 		//fprintf(stderr,"%x, ",response[i]);}
@@ -135,7 +160,7 @@ int read_GPS(struct gps *gpsData_ptr)
 		switch(state) {
 		case 0:
 			if(response[counter]==0xAA){
-				//fprintf(stderr,"Read 0xAA\n");
+				fprintf(stderr,"Read 0xAA\n");
 				state++;
 				counter++;
 			}else{
@@ -145,7 +170,7 @@ int read_GPS(struct gps *gpsData_ptr)
 			break;
 		case 1:
 			if(response[counter]==0x44){
-				//fprintf(stderr,"Read 0x44\n");
+				fprintf(stderr,"Read 0x44\n");
 				state++;
 				counter++;
 			}else{
@@ -155,7 +180,7 @@ int read_GPS(struct gps *gpsData_ptr)
 			break;
 		case 2:
 			if(response[counter]==0x12){
-				//fprintf(stderr,"Read 0x12\n");
+				fprintf(stderr,"Read 0x12\n");
 				state++;
 				//counter++;
 			}else{
@@ -183,37 +208,37 @@ int read_GPS(struct gps *gpsData_ptr)
 				state = 0;
 				counter = 0;
 				//fprintf(stderr,">>CRC Err!\n");
-				gpsData_ptr->badDataCounter++;
+				//gpsData_ptr->badDataCounter++;
 
-				if(gpsData_ptr->badDataCounter > 5)
-				{
+				//if(gpsData_ptr->badDataCounter > 5)
+				//{
 					// reset the GPS!
 					//gpio_set_value(GPS_RST, 0);
 					//gpio_set_value(GPS_RST, 1);
 
-					gpsData_ptr->badDataCounter = 0;
-				}
+					//gpsData_ptr->badDataCounter = 0;
+				//}
 				
 				return -1;
 			}
 
-			memcpy(gpsData_ptr->responseBuffer,&response[counter-2],144);			
+			//memcpy(gpsData_ptr->responseBuffer,&response[counter-2],144);			
 
-			if(!gpio_get_value(GPS_POS_VAL) && ((get_timestamp_ms()-gpsData_ptr->lastPosVal)>300000) && (gpsData_ptr->posValFlag == 1))
-			{
+			//if(!gpio_get_value(GPS_POS_VAL) && ((get_timestamp_ms()-gpsData_ptr->lastPosVal)>300000) && (gpsData_ptr->posValFlag == 1))
+			//{
 				// reset the GPS!
 		//		printf("GPS FLAG1\n");
                                 //gpio_set_value(GPS_RST, 0);
                                 //gpio_set_value(GPS_RST, 1);
-				gpsData_ptr->posValFlag = 0;
-				return -1;
-			}
-			else if(gpio_get_value(GPS_POS_VAL))
-			{
+				//gpsData_ptr->posValFlag = 0;
+				//return -1;
+			//}
+			//else if(gpio_get_value(GPS_POS_VAL))
+			//{
 				// set flag and time
-				gpsData_ptr->posValFlag = 1;
-				gpsData_ptr->lastPosVal = get_timestamp_ms();
-			}
+				//gpsData_ptr->posValFlag = 1;
+				//gpsData_ptr->lastPosVal = get_timestamp_ms();
+			//}
 		
 			pCount = counter - 2;
 			
@@ -223,21 +248,28 @@ int read_GPS(struct gps *gpsData_ptr)
 			//endian_swap(response,pCount+28+16,8);
 			//endian_swap(response,pCount+28+24,8);
 			
-			gpsData_ptr->timeStatus = *((uint8_t *)(&response[13]));
+			//gpsData_ptr->timeStatus = *((uint8_t *)(&response[13]));
 			//printf("GPS Time Status: %d\n",gpsData_ptr->timeStatus);
-			gpsData_ptr->weekRef = *((uint16_t *)(&response[14]));
-			gpsData_ptr->time = *((long *)(&response[16]));	
+			//gpsData_ptr->weekRef = *((uint16_t *)(&response[14]));
+			//gpsData_ptr->time = *((long *)(&response[16]));	
 			// Set system time from GPS if it is good
-        		if(gpsData_ptr->timeStatus == 80 || gpsData_ptr->timeStatus == 100 || gpsData_ptr->timeStatus == 120 ||gpsData_ptr->timeStatus == 140 || gpsData_ptr->timeStatus == 160 || gpsData_ptr->timeStatus == 170 || gpsData_ptr->timeStatus == 180)
-			{
-				time_t unixSeconds = 315964800 + (24*7*3600*gpsData_ptr->weekRef + gpsData_ptr->time/1000);
-				time_t *timeSeconds = &unixSeconds;
-				stime(timeSeconds);	
-			}		
+        		//if(gpsData_ptr->timeStatus == 80 || gpsData_ptr->timeStatus == 100 || gpsData_ptr->timeStatus == 120 ||gpsData_ptr->timeStatus == 140 || gpsData_ptr->timeStatus == 160 || gpsData_ptr->timeStatus == 170 || gpsData_ptr->timeStatus == 180)
+			//{
+				//time_t unixSeconds = 315964800 + (24*7*3600*gpsData_ptr->weekRef + gpsData_ptr->time/1000);
+				//time_t *timeSeconds = &unixSeconds;
+				//stime(timeSeconds);	
+			//}		
 				
-			gpsData_ptr->Xe = *((double *)(&response[36]));
-			gpsData_ptr->Ye = *((double *)(&response[44]));
-			gpsData_ptr->Ze = *((double *)(&response[52]));
+			//gpsData_ptr->Xe = *((double *)(&response[36]));
+			//gpsData_ptr->Ye = *((double *)(&response[44]));
+			//gpsData_ptr->Ze = *((double *)(&response[52]));
+			Xe = *((double *)(&response[36]));
+			Ye = *((double *)(&response[44]));
+			Ze = *((double *)(&response[52]));
+			
+			fprintf(stderr,"X-pos = %d", Xe);
+			fprintf(stderr,"Y-pos = %d", Ye);
+			fprintf(stderr,"Z-pos = %d", Ze);
 
 			state = 0;
 			counter = bytesInLocalBuffer;
@@ -249,7 +281,7 @@ int read_GPS(struct gps *gpsData_ptr)
 		}
 	}
 	
-	return 0;
+	return 1;
 }
 
 
